c6e14d78739f56a26ad633ccce08d85b9954f0f1
Adam Tang <adamtang2004@hotmail.com>
2025-11-07 01:55:01 +0000

    SLURM for 1D

--- HPC_jobs/1D_jobs/1D_SF_job.sh
+++ HPC_jobs/1D_jobs/1D_SF_job.sh
@@ -1,15 +1,16 @@
-#! /bin/bash
+#!/bin/bash
 
-#SBATCH --job-name=1D_PV
-#SBATCH --output=/storage/physics/phugcx/output_%j.log
-#SBATCH --error=/storage/physics/phugcx/error_%j.log
+#SBATCH --job-name=1D_SF
+#SBATCH --output=../../output/logs/%x_output_%j.log
+#SBATCH --error=../../output/logs/%x_error_%j.log
 #SBATCH --time=02:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
-#SBATCH --mem=3988
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=8G
 
+module purge
 module load PyTorch/2.0-Miniconda3-4.12.0-Python-3.10.4
 module load GCC/11.3.0 OpenMPI/4.1.4 SciPy-bundle/2022.05
 module load GCCcore/11.3.0 Python/3.10.4
 
-python /storage/physics/phugcx/1D_Part.py 100 500
+python ../../src/CQW_1D/1D_SF.py 100 500
