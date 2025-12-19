9aba3c54a653354e4033a3baab52b9c2cf413623
Adam Tang <adamtang2004@hotmail.com>
2025-12-19 17:06:11 +0000

    Simplified code using modules

--- src/CQW_1D/1D_PV.py
+++ src/CQW_1D/1D_PV.py
@@ -3,6 +3,8 @@ import pandas as pd
 from numba import prange, njit
 import sys #For scripting runs
 
+from operators import shift_1d, G_1d
+
 # Parameters
 time_steps = [10, 25, 50, 100]
 size = 2 * max(time_steps) + 1
@@ -17,24 +19,6 @@ if len(sys.argv) == 2:
 
 #Evenly spaced disorder strengths
 disorder_strength = np.linspace(0, 1, 11)
-
-# Coin operator with disorder
-def G_gate(disorder, w):
-
-    r_x = 0.5 * (1 + w * disorder)
-
-    G = np.array([
-        [np.sqrt(r_x), np.sqrt(1 - r_x)],
-        [np.sqrt(1 - r_x), -np.sqrt(r_x)]
-    ])
-    return G
-
-# Shift operator
-def apply_shift(state):
-    new_state = np.zeros_like(state)
-    new_state[1:, 0] = state[:-1, 0]
-    new_state[:-1, 1] = state[1:, 1]
-    return new_state
 
 # Data storage
 data = {}
@@ -52,8 +36,8 @@ for t in time_steps:
 
             for step in range(t):
                 for x in range(size):
-                    state[x, :] = np.dot(G_gate(random_disorder[x], W), state[x, :])
-                state = apply_shift(state)
+                    state[x, :] = np.dot(G_1d(random_disorder[x], W), state[x, :])
+                state = shift_1d(state)
 
             participation_value = 1 / (np.sum(np.abs(state)**4) * ((2*t)+1))
             total_participation += participation_value
