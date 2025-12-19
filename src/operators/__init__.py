2526e85351906609cd67fc0901a4cfbd4c9b2e0d
Adam Tang <adamtang2004@hotmail.com>
2025-12-19 16:14:26 +0000

    Create coins and added them to init

--- src/operators/__init__.py
+++ src/operators/__init__.py
@@ -1,5 +1,5 @@
 from .shifts import shift_1d, shift_2d, shift_3d
-from .coins import G_1d
+from .coins import G_1d, G_2d, G_3d
 
 if __name__ == "__main__":
 
--- src/operators/coins.py
+++ src/operators/coins.py
@@ -1,13 +1,55 @@
 import numpy as np
 
-def G_1d(disorder: float, w: float) -> np.ndarray:
+def G_1d(disorder: float, W: float) -> np.ndarray:
 
-    r_x = 0.5 * (1 + w * disorder)
+    r_x = 0.5 * (1 + W * disorder)
 
     G = np.array([
         [np.sqrt(r_x), np.sqrt(1 - r_x)],
         [np.sqrt(1 - r_x), -np.sqrt(r_x)]
-    ])
+    ], dtype=np.complex128)
+    return G
+
+def G_2d(disorder: np.ndarray, W: float) -> np.ndarray:
+
+    rx = 0.5 * (1 + W * disorder[0])
+    ry = 0.5 * (1 + W * disorder[1])
+    
+    Gx = np.array([
+        [np.sqrt(rx), np.sqrt(1-rx)],
+        [np.sqrt(1-rx) , -np.sqrt(rx)]
+    ], dtype=np.complex128)
+
+    Gy = np.array([
+        [np.sqrt(ry), np.sqrt(1-ry)],
+        [np.sqrt(1-ry) , -np.sqrt(ry)]
+    ], dtype=np.complex128)
+
+    G = np.kron(Gx, Gy)
+    return G
+
+def G_3d(disorder: np.ndarray, W: float) -> np.ndarray:
+    
+    rx = 0.5 * (1 + W * disorder[0])
+    ry = 0.5 * (1 + W * disorder[1])
+    rz = 0.5 * (1 + W * disorder[2])
+    
+    Gx = np.array([
+        [np.sqrt(rx), np.sqrt(1 - rx)],
+        [np.sqrt(1 - rx), -np.sqrt(rx)]
+    ], dtype=np.complex128)
+
+    Gy = np.array([
+        [np.sqrt(ry), np.sqrt(1 - ry)],
+        [np.sqrt(1 - ry), -np.sqrt(ry)]
+    ], dtype=np.complex128)
+
+    Gz = np.array([
+        [np.sqrt(rz), np.sqrt(1 - rz)],
+        [np.sqrt(1 - rz), -np.sqrt(rz)]
+    ], dtype=np.complex128)
+    
+    G = np.kron(np.kron(Gx, Gy), Gz)
     return G
 
 if __name__ == "__main__":
